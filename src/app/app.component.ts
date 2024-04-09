090cb6727d6bab6edb978f2aaa9e1a254c286f37
David Gergely <dgergely03@gmail.com>
2024-04-09 16:29:49 +0300

    Initial setup

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component} from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatIconModule} from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, RouterLink],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+}
